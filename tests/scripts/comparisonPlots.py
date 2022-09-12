00139f10001989d2b892b1a0baeadf7558e639a9
Thomas Boudreaux <thomas@boudreauxmail.com>
2022-09-12 09:54:28 -0400

    added residual plot to compaisions plots

--- tests/scripts/comparisonPlots.py
+++ tests/scripts/comparisonPlots.py
@@ -1,10 +1,21 @@
 import matplotlib.pyplot as plt
 from pyTOPSScrape.parse import load_opal
 
+import os
+import datetime
+
 def make_comparision_plot():
     TargetPath = "./GS98Target.opac"
     TestPath = "./GS98TestResult.opac"
     OPALPath = "./GS98OPAL.opac"
+
+    targetTime = datetime.datetime.fromtimestamp(os.path.getmtime(TargetPath))
+    testTime = datetime.datetime.fromtimestamp(os.path.getmtime(TestPath))
+    OPALTime = datetime.datetime.fromtimestamp(os.path.getmtime(OPALPath))
+
+    print(f"Target File Last Modified at: {targetTime}")
+    print(f"Test File Last Modified at: {testTime}")
+    print(f"OPAL Comp File Last Modified at: {OPALTime}")
 
     Target = load_opal(TargetPath)
     Test = load_opal(TestPath)
@@ -20,6 +31,12 @@ def make_comparision_plot():
     ax.set_title("Comparision made at log(R)=-1.5")
     plt.savefig("comparison.pdf", bbox_inches='tight')
 
+    fig, ax = plt.subplots(1,1,figsize=(10,7))
+    ax.plot(Target[0], Target[2][75, :, 13] - Test[2][75, :, 13])
+    ax.set_xlabel("Log T")
+    ax.set_ylabel("Opacity")
+    ax.set_title("Target - Result Residuals made at log(R)=-1.5")
+    plt.savefig("TRResid.pdf", bbox_inches='tight')
 
 
 if __name__ == "__main__":
